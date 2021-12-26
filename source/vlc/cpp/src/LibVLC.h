f815c1c3b2220790c3b241609cf5d55ee72ba691
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2021-12-25 23:04:58 -0800

    Update LibVLC.h

--- source/vlc/cpp/src/LibVLC.h
+++ source/vlc/cpp/src/LibVLC.h
@@ -18,61 +18,60 @@ typedef struct ctx
 
 class LibVLC
 {
-	public:
-		LibVLC();
-		~LibVLC();
-		static LibVLC* create();
-		
-		void setPath(const char * path);
-		void play();
-		void play(const char * path);
-		void playInWindow();
-		void playInWindow(const char * path);
-		void stop();
-		void fullscreen(bool fullscreen);
-		void togglePause();
-		void pause();
-		void resume();
-		libvlc_time_t getLength();
-		libvlc_time_t getDuration();
-		int getWidth();
-		int getHeight();
-		int isPlaying();
-		void useHWacceleration(bool hwAcc);
-		//char *str LibVLC::getMeta(libvlc_media_t media);
-		uint8_t* getPixelData();
-		void setVolume(float volume);
-		float getVolume();
-		//void setCallback(cpp::Function<void (String)> callback, int cbIndex);
-		libvlc_time_t getTime(); // This caused building issues.
-		void setTime(libvlc_time_t time);
-		float getPosition();
-		void setPosition(float pos);
-		bool isSeekable();
-		float getFps();
-		void nextFrame();
-		bool hasVout();
-		void setRepeat(int numRepeats);
-		int getRepeat();
-		const char* getLastError();
-		float getFPS();
-		void openMedia(const char* mediaPathName);
-		int flags[16]={-1};
-		void setFormat(char* chroma, unsigned* width, unsigned* height, unsigned* pitches, unsigned* lines);
-		void setInitProps(); // This caused building issues.
-		t_ctx ctx;
-		
-	private:
-		libvlc_instance_t* libVlcInstance;
-		libvlc_media_t* libVlcMediaItem;
-		libvlc_media_player_t* libVlcMediaPlayer;
-		libvlc_event_manager_t* eventManager;
-        static void callbacks( const libvlc_event_t* event, void* self );
-        void registerEvents();
-		int repeat;
-		int callbackIndex;
-		cpp::Function<Void (String)> vlcCallbackMth;
-		// float vol = 1.0; -- why was this made??? wtf
+public:
+	LibVLC();
+	~LibVLC();
+	static LibVLC *create();
+
+	void setPath(const char *path);
+	void play();
+	void play(const char *path);
+	void playInWindow();
+	void playInWindow(const char *path);
+	void stop();
+	void fullscreen(bool fullscreen);
+	void togglePause();
+	void pause();
+	void resume();
+	libvlc_time_t getLength();
+	libvlc_time_t getDuration();
+	int getWidth();
+	int getHeight();
+	int isPlaying();
+	void useHWacceleration(bool hwAcc);
+
+	uint8_t *getPixelData();
+	void setVolume(float volume);
+	float getVolume();
+
+	libvlc_time_t getTime();
+	void setTime(libvlc_time_t time);
+	float getPosition();
+	void setPosition(float pos);
+	bool isSeekable();
+	float getFps();
+	void nextFrame();
+	bool hasVout();
+	void setRepeat(int numRepeats);
+	int getRepeat();
+	const char *getLastError();
+	float getFPS();
+	void openMedia(const char *mediaPathName);
+	int flags[16] = {-1};
+	void setFormat(char *chroma, unsigned *width, unsigned *height, unsigned *pitches, unsigned *lines);
+	void setInitProps();
+	t_ctx ctx;
+
+private:
+	libvlc_instance_t *libVlcInstance;
+	libvlc_media_t *libVlcMediaItem;
+	libvlc_media_player_t *libVlcMediaPlayer;
+	libvlc_event_manager_t *eventManager;
+	static void callbacks(const libvlc_event_t *event, void *self);
+	void registerEvents();
+	int repeat;
+	int callbackIndex;
+	cpp::Function<Void(String)> vlcCallbackMth;
 };
 
 #endif
