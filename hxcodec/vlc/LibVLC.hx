2da87657bf319978b4ce6eaf543962eec6e2cea2
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2021-12-28 14:35:27 -0800

    sad

--- hxcodec/vlc/LibVLC.hx
+++ hxcodec/vlc/LibVLC.hx
@@ -6,9 +6,8 @@ import cpp.UInt8;
 #end
 
 // Only works with VScode, send me your solutions at https://github.com/Jrgamer4u/FNKNGT/labels/Source-code%20editor%20can%20not%20find%20plugins%20and%20dlls
-// this solution might not work.
 @:buildXml("
-  <lib name='${haxelib:hxcodec}/vlc/LibVLCBuild.xml'/>
+	<include name='C:/HaxeToolkit/haxe/lib/hxcodec/git/hxCodec/vlc/LibVLCBuild.xml' />
   ")
 
 @:include("LibVLC.h")
