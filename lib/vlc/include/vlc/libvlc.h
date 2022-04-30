d609c82b200d54f01611cdaa9d73db6d8dfcd8d8
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-30 09:23:28 +0300

    Fixing the MP4 sprite if you press enter in game + adding the android back button to finish the video in android (#97)
    * some fixes + adding the back button to finish the video
    
    * some changes in MP4Sprite

--- lib/vlc/include/vlc/libvlc.h
+++ lib/vlc/include/vlc/libvlc.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e3dca47c884c6d533b6c08782babebd0087cee98b22411698ae8b8da3dcf38b
-size 20627
+oid sha256:e5b512776405b6afc593e8c145eb3b377727b508c31c85a365d3b2378b4cf9c5
+size 20617
