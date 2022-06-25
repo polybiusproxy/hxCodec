802644202d3ab0022f247ca1bdfc4aef14f7332e
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-06-25 13:47:59 +0300

    x86 support (#145)
    Co-authored-by: Dan Roberts <47796739+polybiusproxy@users.noreply.github.com>

--- lib/vlc/src/LibVLC.cpp
+++ lib/vlc/src/LibVLC.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa034bd283c51ea915e19b22a785613404387b1ef88622b64ccc078ee4614506
-size 8705
+oid sha256:e9f27a242da9419f3d23e0fca51682c4a8fb99298a67f39daa103c51aa5f53a1
+size 8666
--- lib/vlc/src/LibVLC.h
+++ lib/vlc/src/LibVLC.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78eac2f6b4ff3cbec5e664239787315a29da19b2da5c9c81cdfa9ea7ec8a5e35
-size 1127
+oid sha256:027f5592d3c443873030af430a992f84e8b604b63a19d56ec730f9d75a6df09c
+size 1086
