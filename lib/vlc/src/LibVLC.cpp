fdca252077528c3d89298bcb45e25f23db192fde
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-07-20 15:38:25 +0300

    Fix several bugs (#166)

--- lib/vlc/src/LibVLC.cpp
+++ lib/vlc/src/LibVLC.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9f27a242da9419f3d23e0fca51682c4a8fb99298a67f39daa103c51aa5f53a1
-size 8666
+oid sha256:e4a9eadd7463ae4c4969d2dcfbf9d7cb99ede6e92f9a333d8e9049afc0d85da5
+size 8794
