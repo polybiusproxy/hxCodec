e3a49d48abdf57d2a7a3f334d091d749b84a56df
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-19 11:05:25 +0200

    The only way

--- src/vlc/VLCBitmap.hx
+++ src/vlc/VLCBitmap.hx
@@ -76,9 +76,8 @@ class VLCBitmap extends Bitmap
 	static function callbacks(p_event:RawConstPointer<LibVLC_Event_T>, p_data:Star<cpp.Void>):Void
 	{
 		var self:VLCBitmap = untyped __cpp__('reinterpret_cast<VLCBitmap_obj*>(p_data)');
-		var event:LibVLC_Event_T = cast p_event;
 
-		switch (event.type)
+		switch (p_event.type)
 		{
 			case LibVLC_EventType.PlayerPlaying:
 			case LibVLC_EventType.PlayerStopped:
