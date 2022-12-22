ee333b524ae815ef87c31ff2c8ae587914ba57bd
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-22 16:06:02 +0200

    Little fixes + additions (#218)
    · Fixed ``bitmapData`` texture not being updated.
    · Fixed some grammar.
    · On desktop, now ``hxCodec`` can play videos from another dir then the application one.
    · Now the volume can be bigger then 100, if you use the ``VLCBitmap`` alone.

--- lib/vlc/src/LibVLC.h
+++ lib/vlc/src/LibVLC.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9bb76eed9a24be8b78f775f0e647a5595ef9898159a562d9234d94b56a47530
-size 1156
+oid sha256:7b0291e9c4088933ecc31e237cd35c81e9c5bf28dfa4c5ae4add1489fc19868a
+size 1091
