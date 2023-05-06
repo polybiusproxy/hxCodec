01bcb629b159bfb2364ff4f98cf96c718d96cb81
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-05-06 08:52:11 +0300

    Update VideoBitmap.hx

--- src/hxcodec/openfl/VideoBitmap.hx
+++ src/hxcodec/openfl/VideoBitmap.hx
@@ -1,25 +1,3 @@
 package hxcodec.openfl;
 
-import hxcodec.base.IVideoPlayer;
-import hxcodec.vlc.VLCBitmap;
-import openfl.events.Event;
-
-class VideoBitmap extends VLCBitmap
-{
-	public function new():Void
-	{
-		super();
-
-		if (stage != null)
-			onAddedToStage();
-		else
-			addEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
-	}
-
-	// Internal Methods
-	private function onAddedToStage(?e:Event):Void
-	{
-		if (hasEventListener(Event.ADDED_TO_STAGE))
-			removeEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
-	}
-}
+typedef VideoBitmap = hxcodec.vlc.VLCBitmap;
