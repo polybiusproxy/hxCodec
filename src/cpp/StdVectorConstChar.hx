dc1923f3d47b15772ce95001e679b0f994a5d7a6
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-05-09 12:26:16 +0300

    Create StdVectorConstChar.hx

---
+++ src/cpp/StdVectorConstChar.hx
@@ -0,0 +1,18 @@
+package cpp;
+
+@:include('vector')
+@:structAccess
+@:native('std::vector<const char *>')
+extern class StdVectorConstChar
+{
+	@:native('std::vector<const char *>')
+	static function create():StdVectorConstChar;
+
+	function at(index:Int):cpp.ConstCharStar;
+	function back():cpp.ConstCharStar;
+	function data():cpp.RawPointer<cpp.ConstCharStar>;
+	function front():cpp.ConstCharStar;
+	function pop_back():Void;
+	function push_back(value:cpp.ConstCharStar):Void;
+	function size():Int;
+}
