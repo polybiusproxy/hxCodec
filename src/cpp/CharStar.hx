3700ff7d10766341f6a42a3b9ab359e9e48f3aec
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-05-09 12:33:20 +0300

    Update CharStar.hx

--- src/cpp/CharStar.hx
+++ src/cpp/CharStar.hx
@@ -1,59 +1,17 @@
 package cpp;
 
-extern abstract CharStar(cpp.RawPointer<cpp.Char>)
+extern abstract CharStar(cpp.RawPointer<cpp.Char>) to (cpp.RawPointer<cpp.Char>)
 {
-	inline function new(s:String)
-	{
+	inline function new(s:String):Void
 		this = untyped s.__s;
-	}
 
-	/**
-	 * Allocate a new `char *` of the given size.
-	 * @param size The size of the `char *` to allocate.
-	 * @return The resulting `CharStar`.
-	 */
-	public static inline function allocate(size:Int):CharStar
-	{
-		return untyped __cpp__("new char[{0}]", size);
-	}
-
-	/**
-	 * Free a `char *` allocated with `allocate`.
-	 * @param ptr The `char *` to free.
-	 */
-	public static inline function free(ptr:CharStar):Void
-	{
-		untyped __cpp__("delete[] {0}", ptr);
-	}
-
-	/**
-	 * Add implicit casting to string.
-	 * @return The resulting haxe `String`.
-	 */
-	@:to
-	public inline function toString():String
-	{
-		return new String(untyped this);
-	}
-
-	/**
-	 * Add implicit casting to pointer.
-	 * @return The resulting `char *`.
-	 */
-	@:to
-	public inline function toPointer():cpp.RawPointer<cpp.Char>
-	{
-		return untyped this;
-	}
-
-	/**
-	 * Add implicit casting from string.
-	 * @param s The string to cast.
-	 * @return The resulting `CharStar`.
-	 */
 	@:from
-	public static inline function fromString(s:String):CharStar
-	{
+	static public inline function fromString(s:String):CharStar
 		return new CharStar(s);
-	}
+
+	@:to extern public inline function toString():String
+		return new String(untyped this);
+
+	@:to extern public inline function toPointer():cpp.RawPointer<cpp.Char>
+		return this;
 }
