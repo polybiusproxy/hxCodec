49a4a9aa11c601b9bc73159909b6c928661f2b69
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-05-09 12:25:44 +0300

    Update and rename StdVector.hx to StdVectorChar.hx

---
+++ src/cpp/StdVectorChar.hx
@@ -0,0 +1,18 @@
+package cpp;
+
+@:include('vector')
+@:structAccess
+@:native('std::vector<char *>')
+extern class StdVectorChar
+{
+	@:native('std::vector<char *>')
+	static function create():StdVectorChar;
+
+	function at(index:Int):cpp.CharStar;
+	function back():cpp.CharStar;
+	function data():cpp.RawPointer<cpp.CharStar>;
+	function front():cpp.CharStar;
+	function pop_back():Void;
+	function push_back(value:cpp.CharStar):Void;
+	function size():Int;
+}
