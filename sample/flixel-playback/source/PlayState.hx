94f2c6e63bfbb5a1ff18eba6045a372913a8ea34
Rapper GF <84131849+RapperGF@users.noreply.github.com>
2023-05-31 03:59:14 -0400

    Added back the samples to dev branch!
    flixel, openfl, and lime!

---
+++ sample/flixel-playback/source/PlayState.hx
@@ -0,0 +1,83 @@
+package;
+
+import flixel.FlxG;
+import flixel.text.FlxText;
+import flixel.FlxState;
+import hxcodec.flixel.FlxVideo;
+
+class PlayState extends FlxState
+{
+	var video:FlxVideo;
+
+	override public function create():Void
+	{
+		this.bgColor = 0xFFFF00FF;
+
+		var text:FlxText = new FlxText(0, 0, FlxG.width, "");
+		text.setFormat(null, 16, 0xFFFFFFFF, "center");
+
+		text.text += "hxCodec Flixel Sample";
+		text.text += "\nPress Q to play video";
+		text.text += "\n";
+
+		super.create();
+	}
+
+	public override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.keys.justPressed.Q)
+		{
+			video = new FlxVideo();
+			video.autoResize = false;
+			video.play('assets/video.mp4');
+			video.onLogMessage.add(Sys.println);
+		}
+
+		if (FlxG.keys.justPressed.A)
+		{
+			video.pause();
+		}
+
+		if (FlxG.keys.justPressed.S)
+		{
+			video.resume();
+		}
+
+		if (FlxG.keys.justPressed.D)
+		{
+			video.stop();
+		}
+
+		if (FlxG.keys.justPressed.F)
+		{
+			video.time -= 5000;
+		}
+
+		if (FlxG.keys.justPressed.G)
+		{
+			video.time += 5000;
+		}
+
+		if (FlxG.keys.justPressed.H)
+		{
+			video.position = 0.0;
+		}
+
+		if (FlxG.keys.justPressed.J)
+		{
+			video.rate = 0.5;
+		}
+
+		if (FlxG.keys.justPressed.K)
+		{
+			video.rate = 1.0;
+		}
+
+		if (FlxG.keys.justPressed.L)
+		{
+			video.rate = 2.0;
+		}
+	}
+}
---
+++ sample/openfl-playback/source/Main.hx
@@ -0,0 +1,54 @@
+package;
+
+import openfl.events.KeyboardEvent;
+import openfl.display.Sprite;
+import hxcodec.openfl.VideoSprite;
+
+/**
+ * This is a basic example of how to use the VideoSprite class in OpenFL.
+ */
+class Main extends Sprite
+{
+	var video:VideoSprite;
+
+	public function new()
+	{
+		super();
+
+		video = new VideoSprite();
+		video.autoResize = true;
+		addChild(video);
+
+		stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
+	}
+
+	function onKeyDown(event:KeyboardEvent):Void
+	{
+		trace(event);
+
+		switch (event.keyCode)
+		{
+			case 81: // Q
+				video.play('assets/video.mp4');
+
+			case 65: // A
+				video.pause();
+			case 83: // S
+				video.resume();
+			case 68: // D
+				video.stop();
+			case 70: // F
+				video.bitmap.time -= 5000;
+			case 71: // G
+				video.bitmap.time += 5000;
+			case 72: // H
+				video.bitmap.position = 0.0;
+			case 74: // J
+				video.bitmap.rate = 0.5;
+			case 75: // K
+				video.bitmap.rate = 1.0;
+			case 76: // L
+				video.bitmap.rate = 2.0;
+		}
+	}
+}
