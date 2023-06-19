7449515a5a400d74ebb7d1b3c9458e626bf1b5bc
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-06-19 08:33:08 +0000

    Little patch

--- samples/openfl-playback/source/Main.hx
+++ samples/openfl-playback/source/Main.hx
@@ -1,17 +1,56 @@
 package;
 
 import openfl.display.Sprite;
-import hxcodec.openfl.VideoSprite;
+import openfl.events.Event;
+import hxcodec.openfl.Video;
 
 class Main extends Sprite
 {
+	private var video:Video;
+
 	public function new():Void
 	{
 		super();
 
-		var video:VideoSprite = new VideoSprite();
-		video.bitmap.onEndReached.add(video.dispose);
-		video.play('assets/video.mp4');
+		video = new Video();
+		video.onEndReached.add(video.dispose);
 		addChild(video);
+
+		video.play(Sys.getCwd() + 'assets/video.mp4');
+
+		stage.addEventListener(Event.ENTER_FRAME, stage_onActivate);
+		stage.addEventListener(Event.ACTIVATE, stage_onActivate);
+		stage.addEventListener(Event.DEACTIVATE, stage_onDeactivate);
+	}
+
+	private function stage_onEnterFrame(event:Event):Void
+	{
+		var aspectRatio:Float = video.videoWidth / video.videoHeight;
+
+		if (stage.stageWidth / stage.stageHeight > aspectRatio)
+		{
+			// stage is wider than video
+			video.width = stage.stageHeight * aspectRatio;
+			video.height = stage.stageHeight;
+		}
+		else
+		{
+			// stage is taller than video
+			video.width = stage.stageWidth;
+			Video.height = stage.stageWidth * (1 / aspectRatio);
+		}
+
+		video.x = (stage.stageWidth - video.width) / 2;
+		video.y = (stage.stageHeight - video.height) / 2;
+	}
+
+	private function stage_onActivate(event:Event):Void
+	{
+		video.resume();
+	}
+
+	private function stage_onDeactivate(event:Event):Void
+	{
+		video.pause();
 	}
 }
