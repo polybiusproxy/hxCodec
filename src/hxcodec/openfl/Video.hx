e716412e7f21298fb9d781275b2887344141e5c9
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-06-23 10:58:46 +0300

    Update Video.hx

--- src/hxcodec/openfl/Video.hx
+++ src/hxcodec/openfl/Video.hx
@@ -175,7 +175,7 @@ class Video extends Bitmap
 
 		#if mac
 		if (Sys.getEnv("VLC_PLUGIN_PATH") != null)
-			Sys.putEnv("VLC_PLUGIN_PATH", Path.normalize(Sys.getCwd() + '../MacOS/plugins');
+			Sys.putEnv("VLC_PLUGIN_PATH", Path.normalize(Sys.getCwd() + '../MacOS/plugins'));
 		#end
 
 		#if windows
